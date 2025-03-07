2590f282170bf29b31cb7afb182c7f5e35f8bd59
Nodyww <aling900423@gmail.com>
2025-03-07 15:30:26 +0800

    登入/註冊頁面(未處理後端)

---
+++ FinalProject_GameForum/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject_GameForum.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult ForgotPW()
+        {
+            return View();
+        }
+    }
+}
