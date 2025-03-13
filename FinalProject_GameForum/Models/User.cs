c475190e2b72d68c68ebd0c339eaf838dd184610
Siyan3215 <kai321512@gmail.com>
2025-03-13 16:43:05 +0800

    修改 user

--- FinalProject_GameForum/Models/User.cs
+++ FinalProject_GameForum/Models/User.cs
@@ -1,5 +1,49 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalProject_GameForum.Models;
+
+public partial class User
 {
     public string UserId { get; set; } = null!;
 
+    public string Password { get; set; } = null!;
+
+    public string Nickname { get; set; } = null!;
+
+    public string? PhotoUrl { get; set; }
+
+    public string? Gender { get; set; }
+
+    public DateTime? Birthdate { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public DateTime? RegisterDate { get; set; }
+
+    public virtual ICollection<ArticleMessage> ArticleMessages { get; set; } = new List<ArticleMessage>();
+
+    public virtual ICollection<Article> Articles { get; set; } = new List<Article>();
+
+    public virtual ICollection<CustomerProblem> CustomerProblems { get; set; } = new List<CustomerProblem>();
+
+    public virtual ICollection<HistoricalVisitor> HistoricalVisitorOwners { get; set; } = new List<HistoricalVisitor>();
+
+    public virtual ICollection<HistoricalVisitor> HistoricalVisitorVisitors { get; set; } = new List<HistoricalVisitor>();
+
+    public virtual ICollection<NewsMessage> NewsMessages { get; set; } = new List<NewsMessage>();
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual ICollection<Relationship> RelationshipPersonAs { get; set; } = new List<Relationship>();
+
+    public virtual ICollection<Relationship> RelationshipPersonBs { get; set; } = new List<Relationship>();
+
+    public virtual ICollection<ShoppingCart> ShoppingCarts { get; set; } = new List<ShoppingCart>();
+
+    public virtual ICollection<ViewHistory> ViewHistories { get; set; } = new List<ViewHistory>();
 }
