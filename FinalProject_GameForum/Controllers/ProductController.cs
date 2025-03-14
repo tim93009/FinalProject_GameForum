31630d961ce2833e766d69908f58ff6dd40fc15b
tim93009 <tim93009@gmail.com>
2025-03-14 16:57:17 +0800

    串後端中

--- FinalProject_GameForum/Controllers/ProductController.cs
+++ FinalProject_GameForum/Controllers/ProductController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinalProject_GameForum.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProject_GameForum.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly GameForumContext _db;
+
+        public ProductController(GameForumContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Product> objProductList = _db.Products.ToList();
+            return View(objProductList);
         }
     }
 }
