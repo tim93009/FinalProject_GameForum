2a06519b6f5d0a703d7b23aac94d31548d1b9da6
haochien-chi <hahaha28465@gmail.com>
2025-03-18 10:43:14 +0800

    修改News頁面

--- FinalProject_GameForum/Controllers/NewsController.cs
+++ FinalProject_GameForum/Controllers/NewsController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinalProject_GameForum.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProject_GameForum.Controllers
 {
     public class NewsController : Controller
     {
+        private readonly GameForumContext _context;
+        public NewsController(GameForumContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var news = _context.News.ToList();
+            return View(news);
         }
         public IActionResult News()
         {
