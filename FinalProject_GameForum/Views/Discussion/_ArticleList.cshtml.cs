c7079c0730aa5524c7ebf2aa8e8d8ee15174ad7a
SJL <pnyuki159@gmail.com>
2025-03-19 00:44:44 +0800

    看板首頁 切換 文章列表（DiscussionController, DisHome, _ArticleList, _LayoutDis2）

---
+++ FinalProject_GameForum/Views/Discussion/_ArticleList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProject_GameForum.Views.Discussion
+{
+    public class _ArticleListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
