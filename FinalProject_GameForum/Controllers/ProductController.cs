0f39cca8e2d556e41d37066d0d7c5c86b7cd5d3d
tim93009 <tim93009@gmail.com>
2025-03-20 11:35:15 +0800

    修改 商城 商品頁面 商城導覽列 新增 購物車頁面

--- FinalProject_GameForum/Controllers/ProductController.cs
+++ FinalProject_GameForum/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using FinalProject_GameForum.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinalProject_GameForum.Controllers
 {
@@ -12,10 +13,25 @@ namespace FinalProject_GameForum.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? productId)
         {
-            List<Product> objProductList = _db.Products.ToList();
-            return View(objProductList);
+            if (productId == null)
+            {
+                return NotFound("請提供商品 ID");
+            }
+
+            // 查詢單一商品並包含相關資料
+            var product = _db.Products
+                .Include(p => p.ProductCategory) // 包含商品分類
+                .Include(p => p.ProductAdditionalImages) // 包含額外圖片
+                .FirstOrDefault(p => p.ProductId == productId);
+
+            if (product == null)
+            {
+                return NotFound("找不到指定的商品");
+            }
+
+            return View(product);
         }
     }
 }
