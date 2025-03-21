5be9c04979da18c4f245f80947e88cabd13c73cb
Nodyww <aling900423@gmail.com>
2025-03-21 11:09:28 +0800

    完成設定頁面 創造搜尋目前登入的使用者資料

--- FinalProject_GameForum/Models/UserLogin.cs
+++ FinalProject_GameForum/Models/UserLogin.cs
@@ -2,15 +2,29 @@
 using FinalProject_GameForum.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinalProject_GameForum.Models
 {
-    
+
     public static class UserLogin
     {
-       public static string GetUserId(this Controller controller)
+        // 取得現在登入中的使用者資訊
+        public static User GetUserInfo(this Controller controller, GameForumContext context)
         {
-            return controller.HttpContext.User.Claims.FirstOrDefault(u => u.Type == ClaimTypes.NameIdentifier)!.Value;
+            var userID = controller.HttpContext.User.Claims.FirstOrDefault(u => u.Type == ClaimTypes.NameIdentifier)!.Value;
+            var userdb = context.Users.FirstOrDefault(u => u.UserId == userID);
+            if (userdb != null)
+            {
+                var userInfo = context.Users.FirstOrDefault(u => u.UserId == userID);
+                return userInfo!;
+            }
+            else
+            {
+                return null!;
+            }
+            
+           
         }
     }
 }
