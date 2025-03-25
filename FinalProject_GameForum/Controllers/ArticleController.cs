81f096cef54cf1286a2cb38be1190db381dbeb77
Siyan3215 <kai321512@gmail.com>
2025-03-25 14:52:59 +0800

    文章-延伸閱讀完成

--- FinalProject_GameForum/Controllers/ArticleController.cs
+++ FinalProject_GameForum/Controllers/ArticleController.cs
@@ -19,15 +19,62 @@ namespace FinalProject_GameForum.Controllers
         [HttpGet]
         public IActionResult Index(int id)
         {
+            // 查詢當前文章群組
+            ArticleGroup? currentArticleGroup = _context.ArticleGroups
+            .Include(ag => ag.Articles)
+            .FirstOrDefault(ag => ag.ArticleGroupId == id);
+
+            if (currentArticleGroup == null)
+            {
+                return NotFound();
+            }
+
+            // 查詢當前文章群組的文章
             List<Article> articles = _context.Articles
            .Include(a => a.ArticleGroup)
            .Include(a => a.User)
            .Include(a => a.ArticleMessages)
-               .ThenInclude(m => m.User)
+               .ThenInclude(am => am.User)
            .Where(a => a.ArticleGroupId == id)
            .ToList();
 
+            // 查詢同看板同類別的其他文章（延伸閱讀）
+            var relatedArticles = _context.Articles
+                .Where(a => a.ArticleGroup!.DiscussionId == currentArticleGroup.DiscussionId
+                         && a.ArticleGroup.Category == currentArticleGroup.Category
+                         && a.ArticleGroupId != id) // 排除當前文章群組
+                .Take(4) // 限制為 4 篇
+                .Select(a => new
+                {
+                    a.ArticleGroupId,
+                    Title = a.ArticleGroup!.ArticleTitle,
+                    Image = a.ArticleGroup.CoverImage != null
+                        ? $"data:image/jpeg;base64,{Convert.ToBase64String(a.ArticleGroup.CoverImage)}"
+                        : null
+                })
+                .ToList();
+
+            // 不足四篇的情況 
+            if (relatedArticles.Count < 4)
+            {
+                var additionalArticles = _context.Articles
+                    .Where(a => a.ArticleGroup!.DiscussionId == currentArticleGroup.DiscussionId
+                             && a.ArticleGroupId != id)
+                    .Take(4 - relatedArticles.Count)
+                    .Select(a => new
+                    {
+                        a.ArticleGroupId,
+                        Title = a.ArticleGroup!.ArticleTitle,
+                        Image = a.ArticleGroup.CoverImage != null 
+                        ? $"data:image/jpeg;base64,{Convert.ToBase64String(a.ArticleGroup.CoverImage)}" 
+                        : null
+                    })
+                    .ToList();
+                relatedArticles.AddRange(additionalArticles);
+            }
+
             ViewBag.ArticleGroupId = id;
+            ViewBag.RelatedArticles = relatedArticles;
             return View(articles);
         }
 
@@ -38,6 +85,11 @@ namespace FinalProject_GameForum.Controllers
         {
             Article? articleLocation = await _context.Articles
             .FirstOrDefaultAsync(a => a.ArticleId == articleId);
+
+            if (articleLocation == null)
+            {
+                return NotFound();
+            }
 
             // 創建留言
             var message = new ArticleMessage()
